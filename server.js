aa0d86a9703c2a270892d72e505ee8fb8e1b7704
Baraa Youzbashi <bara2@hotmail.co.uk>
2017-08-11 16:15:18 +0300

    Initialize repo

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+
+/*
+	Express app takes two arguments , the first is the route , the second is a function
+	that has request - response parameters 
+
+	the port our server uses must be detirmened in "app.listen()"
+	app.listen also takes a function that executes when the server starts
+
+	we can add middleware on app level or route level
+
+	ex :
+		http://localhost:3000
+		http://localhost:3000/about
+*/
+
+
+var middleware = {
+	requireAuthentication: function (req, res, next){
+		console.log('Private route requested');
+		next();
+	},
+	logger: function(req, res, next){
+		console.log('Request: '+ req.method + ' '+ req.originalUrl + ' @ '+ new Date().toString() );
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+
+// app.get('/',function (req, res) {
+// 	res.send('Hello Express!');
+// });
+
+app.get('/login' ,middleware.requireAuthentication, function(req, res){
+	res.send('Login is for Members only');
+});
+
+app.get('/about',function(req, res){
+	res.send('About Us Page');
+});
+
+app.use(express.static(__dirname+'/public'));
+
+app.listen(PORT,function(){
+	console.log('Server is workin on port '+ PORT);
+});
